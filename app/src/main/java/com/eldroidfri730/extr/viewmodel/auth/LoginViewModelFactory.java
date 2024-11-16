a6d247dd703e63801a36f79ee654e7fff4a1d5df
ajiwnl <alvinjohnaranetz@gmail.com>
2024-11-16 23:25:19 +0800

    login initial

---
+++ app/src/main/java/com/eldroidfri730/extr/viewmodel/auth/LoginViewModelFactory.java
@@ -0,0 +1,25 @@
+package com.eldroidfri730.extr.viewmodel.auth;
+
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+    private final Application application;
+
+    public LoginViewModelFactory(Application application) {
+        this.application = application;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(application);
+        }
+        throw new IllegalArgumentException("Unknown ViewModel class");
+    }
+}
