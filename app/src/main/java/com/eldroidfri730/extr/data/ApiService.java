1155a13443541257e175b2a25ff25bbe91ddc7d3
Edz Henissey Son <henisseyson@gmail.com>
2024-11-22 23:29:44 +0800

    Categories

--- app/src/main/java/com/eldroidfri730/extr/data/ApiService.java
+++ app/src/main/java/com/eldroidfri730/extr/data/ApiService.java
@@ -1,11 +1,17 @@
 package com.eldroidfri730.extr.data;
 
+import com.eldroidfri730.extr.data.models.mCategory;
 import com.eldroidfri730.extr.data.models.mUser;
 import com.eldroidfri730.extr.data.response.LoginResponse;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ApiService {
 
@@ -17,4 +23,17 @@ public interface ApiService {
 
     @POST("password/forgot")
     Call<mUser> forgotPassword(@Body mUser user);
+
+    @POST("userCategories")
+    Call<List<mCategory>> getUserCategories(@Body mUser user);
+
+    @POST("addCategory")
+    Call<mCategory> addCategory(@Body mCategory category);
+
+    @PATCH("updateCategory/{categoryTitle}")
+    Call<mCategory> updateCategory(@Path("categoryTitle") String categoryTitle, @Body mCategory category);
+
+    @DELETE("deleteCategory/{categoryTitle}")
+    Call<mCategory> deleteCategory(@Path("categoryTitle") String categoryTitle, @Body mCategory category);
+
 }
