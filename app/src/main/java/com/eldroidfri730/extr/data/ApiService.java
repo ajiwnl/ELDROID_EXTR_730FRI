8ef8cc31ae94005e24fc2004906fa67663d4f1a2
Soliken <155744710+soliken1@users.noreply.github.com>
2024-11-26 10:28:42 +0800

    Merge

--- app/src/main/java/com/eldroidfri730/extr/data/ApiService.java
+++ app/src/main/java/com/eldroidfri730/extr/data/ApiService.java
@@ -75,14 +75,12 @@ public interface ApiService {
     @POST("userBudgets")
     Call<List<mBudget>> getBudgetByUserId(@Query("userId") String userId);
 
-    @FormUrlEncoded
     @PATCH("updateBudget/{categoryTitle}")
     Call<mBudget> patchBudget(
             @Path("categoryTitle") String categoryTitle,
-            @Field("userId") String userId,
-            @Field("amount") String addAmount
-
+            @Body mBudget budget
     );
+
 
     @POST("addExpense")
     Call<mExpense> addExpense(@Body mExpense expense);
